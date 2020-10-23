e465aa63ac762f52e0d73144b49f6c379a098d3f
Mauro Stettler <mauro.stettler@gmail.com>
2020-10-23 18:17:55 +0000

    generate chunk id

---
+++ cmd/chunkid/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/prometheus/prometheus/pkg/labels"
+
+	"github.com/cortexproject/cortex/pkg/chunk"
+	"github.com/prometheus/common/model"
+)
+
+func main() {
+	userID := flag.String("user", "", "user id as string")
+	labelSetStr := flag.String("labelSet", "", "full labelset of metric, where name is value of label __name__, labels separated by \":\"")
+	fromI := flag.Int64("from", 0, "from timestamp in ms")
+	throughI := flag.Int64("through", 0, "through timestamp in ms")
+	flag.Parse()
+
+	builder := labels.NewBuilder(nil)
+	for _, label := range strings.SplitN(*labelSetStr, ":", 2) {
+		lv := strings.SplitN(label, "=", 2)
+		if len(lv) != 2 {
+			panic(fmt.Sprintf("invalid label/value: %s", lv))
+		}
+
+		builder.Set(lv[0], lv[1])
+	}
+
+	labels := builder.Labels()
+	labelSet := make(model.LabelSet)
+
+	for _, l := range labels {
+		labelSet[model.LabelName(l.Name)] = model.LabelValue(l.Value)
+	}
+
+	c := chunk.Chunk{
+		UserID:      *userID,
+		Metric:      labels,
+		Fingerprint: labelSet.Fingerprint(),
+		From:        model.Time(*fromI),
+		Through:     model.Time(*throughI),
+	}
+
+	fmt.Println(fmt.Sprintf("Chunk ID is: %s", c.ExternalKey()))
+}
