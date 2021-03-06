4715c9a6632790ecc199994e8a5ca2cd06d7fdcb
Jacob Lisi <jacob.t.lisi@gmail.com>
2021-03-05 22:44:26 -0500

    chore(benchtool): use Go 1.16
    Signed-off-by: Jacob Lisi <jacob.t.lisi@gmail.com>

--- cmd/benchtool/main.go
+++ cmd/benchtool/main.go
@@ -34,23 +34,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	ctx := context.Background()
-
-	// trap Ctrl+C and call cancel on the context
-	ctx, cancel := context.WithCancel(ctx)
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	defer func() {
-		signal.Stop(c)
-		cancel()
-	}()
-	go func() {
-		select {
-		case <-c:
-			cancel()
-		case <-ctx.Done():
-		}
-	}()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
 	go func() {
 		http.Handle("/metrics", promhttp.Handler())
