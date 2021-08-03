965825470bb72464a23907cd063b32691fe4d75e
Callum Jones <callum.jones@airbnb.com>
2021-08-03 13:11:35 -0700

    [e2ealerting] Support pass through labels
    Allow for customizing the labels included in the final end_to_end_duration_seconds histogram, allowing multiple probs to be deployed and the latency split by labels forwarded to it from AlertManager.

--- pkg/alerting/receiver_test.go
+++ pkg/alerting/receiver_test.go
@@ -22,7 +22,7 @@ func Test_measureLatency(t *testing.T) {
 		name    string
 		alerts  template.Data
 		err     error
-		tracked []float64
+		tracked []timestampKey
 	}{
 		{
 			name: "with alerts to track",
@@ -33,6 +33,18 @@ func Test_measureLatency(t *testing.T) {
 						Annotations: template.KV{"time": "1.604069614e+09"},
 						Status:      string(model.AlertFiring),
 					},
+					// duplicate alert, will be ignored
+					template.Alert{
+						Labels:      template.KV{model.AlertNameLabel: "e2ealertingAlwaysFiring"},
+						Annotations: template.KV{"time": "1.604069614e+09"},
+						Status:      string(model.AlertFiring),
+					},
+					// different alert at same time
+					template.Alert{
+						Labels:      template.KV{model.AlertNameLabel: "ADifferentAlertAtTheSameTime"},
+						Annotations: template.KV{"time": "1.604069614e+09"},
+						Status:      string(model.AlertFiring),
+					},
 					template.Alert{
 						Labels:      template.KV{model.AlertNameLabel: "e2ealertingAlwaysFiring"},
 						Annotations: template.KV{"time": "1.604069615e+09"},
@@ -40,7 +52,23 @@ func Test_measureLatency(t *testing.T) {
 					},
 				},
 			},
-			tracked: []float64{1604069614.00, 1604069615.00},
+			tracked: []timestampKey{
+				{
+					alertName:   "e2ealertingAlwaysFiring",
+					labelValues: "",
+					timestamp:   1604069614.00,
+				},
+				{
+					alertName:   "ADifferentAlertAtTheSameTime",
+					labelValues: "",
+					timestamp:   1604069614.00,
+				},
+				{
+					alertName:   "e2ealertingAlwaysFiring",
+					labelValues: "",
+					timestamp:   1604069615.00,
+				},
+			},
 		},
 		{
 			name: "with alerts that don't have a time annotation or alertname label it ignores them",
@@ -61,7 +89,7 @@ func Test_measureLatency(t *testing.T) {
 					},
 				},
 			},
-			tracked: []float64{1604069614.00},
+			tracked: []timestampKey{{alertName: "e2ealertingAlwaysFiring", labelValues: "", timestamp: 1604069614.00}},
 		},
 	}
 
@@ -87,9 +115,118 @@ func Test_measureLatency(t *testing.T) {
 
 			require.Equal(t, http.StatusOK, w.Code)
 			require.Equal(t, len(tt.tracked), len(r.timestamps))
-			for _, timestamp := range tt.tracked {
-				_, exists := r.timestamps[timestamp]
-				require.True(t, exists, fmt.Sprintf("time %f is not tracked", timestamp))
+			for _, key := range tt.tracked {
+				_, exists := r.timestamps[key]
+				require.True(t, exists, fmt.Sprintf("time %f is not tracked", key.timestamp))
+			}
+		})
+	}
+}
+
+func Test_measureLatencyWithAdditionalLabels(t *testing.T) {
+	tc := []struct {
+		name    string
+		alerts  template.Data
+		err     error
+		tracked []timestampKey
+	}{
+		{
+			name: "with alerts to track",
+			alerts: template.Data{
+				Alerts: template.Alerts{
+					template.Alert{
+						Labels:      template.KV{model.AlertNameLabel: "e2ealertingAlwaysFiring", "region": "us-east-1"},
+						Annotations: template.KV{"time": "1.604069614e+09"},
+						Status:      string(model.AlertFiring),
+					},
+					// duplicate alert, will be ignored
+					template.Alert{
+						Labels:      template.KV{model.AlertNameLabel: "e2ealertingAlwaysFiring", "region": "us-east-1"},
+						Annotations: template.KV{"time": "1.604069614e+09"},
+						Status:      string(model.AlertFiring),
+					},
+					// different alert at same time
+					template.Alert{
+						Labels:      template.KV{model.AlertNameLabel: "e2ealertingAlwaysFiring", "region": "us-west-1"},
+						Annotations: template.KV{"time": "1.604069614e+09"},
+						Status:      string(model.AlertFiring),
+					},
+					template.Alert{
+						Labels:      template.KV{model.AlertNameLabel: "e2ealertingAlwaysFiring", "region": "us-east-1"},
+						Annotations: template.KV{"time": "1.604069615e+09"},
+						Status:      string(model.AlertFiring),
+					},
+				},
+			},
+			tracked: []timestampKey{
+				{
+					alertName:   "e2ealertingAlwaysFiring",
+					labelValues: "us-east-1",
+					timestamp:   1604069614.00,
+				},
+				{
+					alertName:   "e2ealertingAlwaysFiring",
+					labelValues: "us-west-1",
+					timestamp:   1604069614.00,
+				},
+				{
+					alertName:   "e2ealertingAlwaysFiring",
+					labelValues: "us-east-1",
+					timestamp:   1604069615.00,
+				},
+			},
+		},
+		{
+			name: "with alerts that don't have a time annotation or alertname label it ignores them",
+			alerts: template.Data{
+				Alerts: template.Alerts{
+					template.Alert{
+						Labels:      template.KV{model.AlertNameLabel: "e2ealertingAlwaysFiring"},
+						Annotations: template.KV{"time": "1.604069614e+09"},
+						Status:      string(model.AlertFiring),
+					},
+					template.Alert{
+						Labels: template.KV{model.AlertNameLabel: "e2ealertingAlwaysFiring"},
+						Status: string(model.AlertFiring),
+					},
+					template.Alert{
+						Annotations: template.KV{"time": "1.604069614e+09"},
+						Status:      string(model.AlertFiring),
+					},
+				},
+			},
+			tracked: []timestampKey{{alertName: "e2ealertingAlwaysFiring", labelValues: "", timestamp: 1604069614.00}},
+		},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			r, err := NewReceiver(
+				ReceiverConfig{
+					PurgeInterval:   1 * time.Hour,
+					LabelsToForward: []string{"region"},
+				},
+				log.NewNopLogger(),
+				prometheus.NewRegistry(),
+			)
+			require.NoError(t, err)
+
+			router := mux.NewRouter()
+			r.RegisterRoutes(router)
+
+			b, err := json.Marshal(tt.alerts)
+			require.NoError(t, err)
+
+			req := httptest.NewRequest(http.MethodPost, "/api/v1/receiver", bytes.NewBuffer(b))
+			w := httptest.NewRecorder()
+
+			router.ServeHTTP(w, req)
+
+			require.Equal(t, http.StatusOK, w.Code)
+			require.Equal(t, len(tt.tracked), len(r.timestamps))
+			for _, key := range tt.tracked {
+				_, exists := r.timestamps[key]
+				require.True(t, exists, fmt.Sprintf("time %f is not tracked", key.timestamp))
 			}
 		})
 	}
