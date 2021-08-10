626c9aa2a2d392187ba76bdbab252c26339eb4fd
Callum Jones <callum.jones@airbnb.com>
2021-08-09 18:26:46 -0700

    new tests

--- pkg/alerting/receiver_test.go
+++ pkg/alerting/receiver_test.go
@@ -231,3 +231,97 @@ func Test_measureLatencyWithAdditionalLabels(t *testing.T) {
 		})
 	}
 }
+
+func Test_measureLatencyWithDifferentLabels(t *testing.T) {
+	tc := []struct {
+		name    string
+		alerts  template.Data
+		err     error
+		tracked []timestampKey
+	}{
+		{
+			name: "with alerts to track",
+			alerts: template.Data{
+				Alerts: template.Alerts{
+					template.Alert{
+						Labels:      template.KV{model.AlertNameLabel: "e2ealertingAlwaysFiring", "region": "us-east-1", "host": "a"},
+						Annotations: template.KV{"time": "1.604069614e+09"},
+						Status:      string(model.AlertFiring),
+					},
+					// different host, will be counted
+					template.Alert{
+						Labels:      template.KV{model.AlertNameLabel: "e2ealertingAlwaysFiring", "region": "us-east-1", "host": "b"},
+						Annotations: template.KV{"time": "1.604069614e+09"},
+						Status:      string(model.AlertFiring),
+					},
+					// different alert at same time
+					template.Alert{
+						Labels:      template.KV{model.AlertNameLabel: "e2ealertingAlwaysFiring", "region": "us-west-1", "host": "c"},
+						Annotations: template.KV{"time": "1.604069614e+09"},
+						Status:      string(model.AlertFiring),
+					},
+					template.Alert{
+						Labels:      template.KV{model.AlertNameLabel: "e2ealertingAlwaysFiring", "region": "us-east-1", "host": "d"},
+						Annotations: template.KV{"time": "1.604069615e+09"},
+						Status:      string(model.AlertFiring),
+					},
+				},
+			},
+			tracked: []timestampKey{
+				{
+					alertName:   "e2ealertingAlwaysFiring",
+					labelValues: "us-east-1a",
+					timestamp:   1604069614.00,
+				},
+				{
+					alertName:   "e2ealertingAlwaysFiring",
+					labelValues: "us-east-1b",
+					timestamp:   1604069614.00,
+				},
+				{
+					alertName:   "e2ealertingAlwaysFiring",
+					labelValues: "us-west-1c",
+					timestamp:   1604069614.00,
+				},
+				{
+					alertName:   "e2ealertingAlwaysFiring",
+					labelValues: "us-east-1d",
+					timestamp:   1604069615.00,
+				},
+			},
+		},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			r, err := NewReceiver(
+				ReceiverConfig{
+					PurgeInterval:   1 * time.Hour,
+					LabelsToTrack:   []string{"region", "host"},
+					LabelsToForward: []string{"region"},
+				},
+				log.NewNopLogger(),
+				prometheus.NewRegistry(),
+			)
+			require.NoError(t, err)
+
+			router := mux.NewRouter()
+			r.RegisterRoutes(router)
+
+			b, err := json.Marshal(tt.alerts)
+			require.NoError(t, err)
+
+			req := httptest.NewRequest(http.MethodPost, "/api/v1/receiver", bytes.NewBuffer(b))
+			w := httptest.NewRecorder()
+
+			router.ServeHTTP(w, req)
+
+			require.Equal(t, http.StatusOK, w.Code)
+			require.Equal(t, len(tt.tracked), len(r.timestamps))
+			for _, key := range tt.tracked {
+				_, exists := r.timestamps[key]
+				require.True(t, exists, fmt.Sprintf("time %f %s is not tracked", key.timestamp, key.labelValues))
+			}
+		})
+	}
+}
